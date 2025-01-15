3d19e2892b35050715cf41b0eff9f965a47de673
talysondutra1 <talysondutrademacena@gmail.com>
2025-01-15 20:37:39 -0300

    Adicionar arquivos de projeto.

---
+++ Desafio_Exercicio_Cpt_Udemy/Conta.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace Desafio_Exercicio_Cpt_Udemy
+{
+     class Conta
+    {
+        public string Titular { get; set; }
+        public int Numero { get; private set; }   
+        public double Saldo {  get; private set; }      
+        public Conta (string nome, int numero) { 
+            Titular = nome;
+            Numero = numero;            
+        }
+        public Conta (string nome, int numero, double saldo) : this(nome, numero) 
+        {
+                Saldo = saldo;
+        }
+        public string Nome
+        {
+            get { return Titular; }
+            set 
+            {
+                if (value != null && value.Length > 1) 
+                {
+                    Titular = value;
+                }
+            }
+        }
+        public void Deposito(double quantia) 
+        {
+            Saldo += quantia;
+        }
+        public void Saque(double quantia) 
+        {
+            Saldo -= quantia + 5.0;
+        }
+        public override string ToString()
+        {
+            return "conta " + Numero + ", Titular: " + Titular + ", Saldo: $" + Saldo.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ Desafio_Exercicio_Cpt_Udemy/Program.cs
@@ -0,0 +1,50 @@
+﻿using Desafio_Exercicio_Cpt_Udemy;
+using System;
+using System.Globalization;
+
+namespace MyApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Conta conta;
+
+            Console.WriteLine("entre com o número da conta: ");
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine("entre com o titular da conta: ");
+            string t = Console.ReadLine();
+            Console.WriteLine("haverá depósito inicial (s/n)? ");
+            char d = char.Parse(Console.ReadLine());
+
+            if (d == 's' || d == 'S')
+            {
+                Console.WriteLine("entre com o valor de depósito inicial: ");
+                double valor = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                conta = new Conta(t, n, valor);
+            }
+            else
+            {       
+                 conta = new Conta(t, n);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Dados da conta:");
+            Console.WriteLine(conta);
+
+            Console.WriteLine();
+            Console.Write("Entre um valor para depósito: ");
+            double quantia = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            conta.Deposito(quantia);
+            Console.WriteLine("Dados da conta atualizados:");
+            Console.WriteLine(conta);
+
+            Console.WriteLine();
+            Console.Write("Entre um valor para saque: ");
+            quantia = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            conta.Saque(quantia);
+            Console.WriteLine("Dados da conta atualizados:");
+            Console.WriteLine(conta);
+        }
+    }
+}
